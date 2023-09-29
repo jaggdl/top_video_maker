71c7f7ef6bc9f1decb04913e7120846f1cbcd017
jaggdl <ja125garcia@gmail.com>
2023-09-28 22:15:30 -0600

    Video builder refactor

--- index.js
+++ index.js
@@ -1,46 +1,14 @@
-import path from 'path'
-import url from 'url';
-import { Video } from './video_maker/video.js'
-import getRandomTrackLongerThan from './video_maker/utils/randomTrackLongerThan.js'
+import { buildVideo } from './video_maker/videoBuilder.js';
 import { uploadVideo } from './uploaders/uploadVideo.js';
 import { uploadVideoToTiktok } from './uploaders/uploadVideoToTiktok.js';
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
+const videoInstance = await buildVideo({
+  subject: 'autos deportivos de la década de los 10s',
+  listLength: 5,
+  asyncItemsBuild: true,
+});
 
-const TOP_LIST_TITLE = 'autos deportivos de la década de los 00s';
-const TOP_LIST_LENGTH = 5;
-const PROJECT_PATH = `./out/${TOP_LIST_TITLE}`;
-const outputDirectory = path.join(__dirname, `${PROJECT_PATH}`);
-const generateItemsAsync = true;
-
-const videoInstance = new Video(TOP_LIST_TITLE, TOP_LIST_LENGTH, outputDirectory);
-await videoInstance.generateStructure();
-
-if (generateItemsAsync) {
-  await videoInstance.generateItemsVideos();
-} else {
-  // Both can be run async
-  await Promise.all([
-    videoInstance.generateNarrationAudio(),
-    videoInstance.generateItemsImages()
-  ])
-
-  await videoInstance.createItemsVideos({
-    concurrentItems: Math.ceil(videoInstance.items.length / 2)
-  });
-}
-
-
-await videoInstance.mergeItemsVideos();
-
-const videoMusicTrack = await getRandomTrackLongerThan(videoInstance.totalDuration);
-console.log('Adding music track:', videoMusicTrack);
-await videoInstance.mixAudioIntoVideo(videoMusicTrack);
-console.log('📹 Master video generated:', videoInstance.masterVideoPath);
-
-console.log(videoInstance.fullDescription); 
 
 // await uploadVideoToTiktok(videoInstance.videoPath, videoInstance.title, videoInstance.hashtags.map(hash => hash.replace('#', '')));
 // await uploadVideo(videoInstance.masterVideoPath, videoInstance.title, videoInstance.fullDescription);
---
+++ video_maker/videoBuilder.js
@@ -0,0 +1,44 @@
+import path from 'path';
+import url from 'url';
+import { Video } from './video.js';
+import getRandomTrackLongerThan from './utils/randomTrackLongerThan.js';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export async function buildVideo({
+  subject,
+  listLength = 5,
+  asyncItemsBuild = true
+}) {
+  const projectPath = `../out/${subject}`;
+  const outputDirectory = path.join(__dirname, `${projectPath}`);
+  const videoInstance = new Video(subject, listLength, outputDirectory);
+  await videoInstance.generateStructure();
+  
+  if (asyncItemsBuild) {
+    await videoInstance.generateItemsVideos();
+  } else {
+    // Both can be run async
+    await Promise.all([
+      videoInstance.generateNarrationAudio(),
+      videoInstance.generateItemsImages()
+    ])
+  
+    await videoInstance.createItemsVideos({
+      concurrentItems: Math.ceil(videoInstance.items.length / 2)
+    });
+  }
+  
+  
+  await videoInstance.mergeItemsVideos();
+  
+  const videoMusicTrack = await getRandomTrackLongerThan(videoInstance.totalDuration);
+  console.log('Adding music track:', videoMusicTrack);
+  await videoInstance.mixAudioIntoVideo(videoMusicTrack);
+  console.log('📹 Master video generated:', videoInstance.masterVideoPath);
+  
+  console.log(videoInstance.fullDescription);
+
+  return videoInstance;
+};
